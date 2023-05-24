2b5c7f58193d2099cd08a3276ce7c99ee80f4ad2
spynetS <alfred@stensatter.se>
2023-05-24 15:31:20 +0200

    fixd raycast

--- Assets/Player/Attack.cs
+++ Assets/Player/Attack.cs
@@ -8,6 +8,7 @@ public class Attack : MonoBehaviour
     public Tool tool;
     public int timer = 100000;
     public float hitdistance = 5;
+    public Transform camera;
     void FixedUpdate()
     {
         if (!Cursor.visible && Input.GetMouseButtonDown(0)){ // if mouse is down
@@ -18,8 +19,10 @@ public class Attack : MonoBehaviour
             // if there is a resource in range and the timer is right 
             RaycastHit hit;
             // Does the ray intersect any objects excluding the player layer
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, hitdistance) && timer > tool.timeBetweenHits)
+            Debug.DrawRay(camera.position, camera.TransformDirection(Vector3.forward));
+            if (Physics.Raycast(camera.position, camera.TransformDirection(Vector3.forward), out hit, hitdistance) && timer > tool.timeBetweenHits)
             {
+                Debug.Log(hit.transform.tag);
                 hit.transform.GetComponentInParent<Resource>().getDamage(tool);
                 timer = 0;
             }
