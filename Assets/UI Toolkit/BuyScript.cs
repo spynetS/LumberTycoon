b534e840a37d988c3f0b7a7954ef55e1318f5f7c
spynetS <you@example.com>
2023-05-30 16:40:48 +0200

    update

--- Assets/UI Toolkit/BuyScript.cs
+++ Assets/UI Toolkit/BuyScript.cs
@@ -5,17 +5,7 @@ using UnityEngine;
 public class BuyScript : MonoBehaviour
 {
     public Canvas canvas;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
     public void BuyLayout()
     {
         canvas.gameObject.SetActive(true);
