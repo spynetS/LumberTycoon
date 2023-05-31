01737db3d1d6b54a6d4032282d00a09b80685e04
spynetS <alfred@stensatter.se>
2023-05-31 15:33:48 +0200

    added spawner

---
+++ Assets/AiSpawner.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class AiSpawner : MonoBehaviour
+{
+
+    public int amount = 1;
+    public GameObject prefab;
+    
+    public List<GameObject> ais;
+    public Vector2 area = new Vector2(10,10);
+
+    private void Update()
+    {
+        foreach (GameObject g in ais)
+        {
+            if (g == null) ais.Remove(g);
+        }
+        if (ais.Count < amount)
+        { 
+            GameObject g = Instantiate(prefab, transform);
+            g.transform.position += new Vector3(Random.Range(-area.x,area.x), g.transform.position.y, Random.Range(-area.y,area.y));
+            ais.Add(g);
+            
+        }
+    }
+}
